9b311735991f2d0cce8f94b2c9bd8fb69fdc5114
hari <harikn77@gmail.com>
2023-02-10 04:15:22 +0530

    File name bug fix!

--- create_metadata.py
+++ create_metadata.py
@@ -8,7 +8,7 @@ file_names = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir
 manufacturer = {}
 for file_name in file_names:
     name = file_name.split('.')[0]
-    manufacturer[name] = file_name
+    manufacturer[name] = os.path.join(dir_path, file_name)
 
 # for each row in the dataframe, check if the image exists in the data folder 
 # if it does, add the path to the image to the dataframe 
