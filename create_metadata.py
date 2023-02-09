d1a3c2f4008612a4862721045a8da3e372399f43
hari <harikn77@gmail.com>
2023-02-10 04:11:48 +0530

    File name bug fix

--- create_metadata.py
+++ create_metadata.py
@@ -4,11 +4,15 @@ import os
 df = pd.read_csv('./laptops.csv')
 
 dir_path = './data'
-file_names = [f.split('.')[0] for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+file_names = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+manufacturer = {}
+for file_name in file_names:
+    name = file_name.split('.')[0]
+    manufacturer[name] = file_name
 
 # for each row in the dataframe, check if the image exists in the data folder 
 # if it does, add the path to the image to the dataframe 
 # if it doesn't, add default image path to the dataframe
 
-df['file_path'] = df.apply(lambda row: os.path.join(dir_path, row['Manufacturer'] + '.jpg') if row['Manufacturer'] in file_names else os.path.join(dir_path, 'default.jpg'), axis=1)
+df['file_path'] = df['Manufacturer'].apply(lambda x: manufacturer[x] if x in manufacturer else 'default.jpg')
 df.to_csv('./metadata.csv', index=False)
